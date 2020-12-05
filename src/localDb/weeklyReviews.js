2f7e5b4424eeb08f9748d28cd723d63e001713e3
Chamara <chamara@spritzer.app>
2020-12-05 17:07:44 +0530

    further inprovements

--- src/localDb/weeklyReviews.js
+++ src/localDb/weeklyReviews.js
@@ -1,25 +1,5 @@
 export const weeklyReviews = [
   {
-    id: "c3e20681-784f-4ea6-a9ec-b77432045bd3",
-    week: {
-      start: "2020-12-01T05:30:00+05:30",
-      end: "2020-12-07T05:30:00+05:30",
-    },
-    teamLeader: { code: "A100", name: "Chamara madhushanka" },
-    recruitment: { existing: "6", registrations: "15", newCodes: "10" },
-    quotations: { target: "30", actual: "22" },
-    activeAdvisors: "6",
-    pendingClearance: {
-      proposals: "9",
-      suspense: "120000",
-      renewals: "150000",
-      revivals: "30000",
-    },
-    meetingNotes: [],
-    status: "pending",
-    actionTime: "",
-  },
-  {
     id: "ff26880f-10ff-4d72-a0ee-8a070b6cf026",
     week: {
       start: "2020-12-08T05:30:00+05:30",
@@ -35,7 +15,53 @@ export const weeklyReviews = [
       renewals: "25000",
       revivals: "120000",
     },
-    meetingNotes: [],
+    meetingNotes: [
+      {
+        id: "86f0027c-2f8f-41ae-b06a-483380afdd79",
+        note: "New advisor \ntraining",
+        status: "completed",
+        createdAt: "2020-12-05T16:41:23+05:30",
+      },
+    ],
+    status: "pending",
+    actionTime: "",
+  },
+  {
+    id: "c3e20681-784f-4ea6-a9ec-b77432045bd3",
+    week: {
+      start: "2020-12-01T05:30:00+05:30",
+      end: "2020-12-07T05:30:00+05:30",
+    },
+    teamLeader: { code: "A100", name: "Chamara madhushanka" },
+    recruitment: { existing: "6", registrations: "15", newCodes: "10" },
+    quotations: { target: "30", actual: "27" },
+    activeAdvisors: "6",
+    pendingClearance: {
+      proposals: "9",
+      suspense: "120000",
+      renewals: "150000",
+      revivals: "30000",
+    },
+    meetingNotes: [
+      {
+        id: "4fd0c852-8aaf-4900-a0a2-6754f1d7c4ab",
+        note: "Recruitment program at \nschool",
+        status: "completed",
+        createdAt: "2020-12-05T16:39:53+05:30",
+      },
+      {
+        id: "b871c1b0-22c6-4495-a356-a6c8457ec564",
+        note: "Leads campaign",
+        status: "fail",
+        createdAt: "2020-12-05T16:40:36+05:30",
+      },
+      {
+        id: "3e84dcf2-cbde-4f06-9ad2-9ce070975af9",
+        note: "New advisor training",
+        status: "todo",
+        createdAt: "2020-12-05T16:40:58+05:30",
+      },
+    ],
     status: "pending",
     actionTime: "",
   },
