b5f85ce5a4bc9696b6a4657fe0b9d5f72fa2cc6e
Chamara <chamara@spritzer.app>
2020-12-05 16:38:06 +0530

    bugs fixing

--- src/localDb/teamLeaders.js
+++ src/localDb/teamLeaders.js
@@ -1,5 +1,5 @@
 export const teamLeaders = [
-  { code: "A100", name: "Chamara madhushanka" },
+  { code: "A100", name: "Chamara Liyanage" },
   { code: "B200", name: "Nisal Adikari" },
   { code: "C100", name: "Chaminda Perera" },
 ];
